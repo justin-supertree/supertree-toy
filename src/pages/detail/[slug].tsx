f12aed9f4b54276602ae48099a651c159f37809c
Justin_supertree <justin1688@supertree.co>
2022-06-29 14:18:48 +0900

    feat: error layout component & api loading state

--- src/pages/detail/[slug].tsx
+++ src/pages/detail/[slug].tsx
@@ -16,6 +16,7 @@ import MetaTag from '@/components/MetaTag';
 import DeleteModal from '@/components/Modal/DeleteModal';
 import UploadModal from '@/components/Modal/UploadModal';
 import DetailLayout from '@/components/Layout/DetailLayout';
+import Loading from '@/components/Loading';
 
 type Prop = {
   noticeId?: number;
@@ -162,6 +163,9 @@ const DetailContent = ({ noticeId }: Props) => {
   const [isRemove, setIsRemove] = useState(0);
   const [contents, setContents] = useState('');
   const [selected, setSelected] = useState('');
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
   const inputElement = useRef<HTMLInputElement>(null);
 
   const selectList = ['service', 'tip', 'event'];
@@ -202,11 +206,13 @@ const DetailContent = ({ noticeId }: Props) => {
         if (response && response.status === 200) {
           handleUploadOpenModal(false);
           handleEdit(false);
+          setIsLoading(false);
           router.push('/');
         }
       });
     } catch (e) {
       console.log(e);
+      setIsLoading(true);
     }
   };
 
@@ -217,13 +223,15 @@ const DetailContent = ({ noticeId }: Props) => {
       }).then((response) => {
         if (response && response.status === 200) {
           const result = response.status;
-          setIsRemove(result);
           router.push('/');
+          setIsRemove(result);
+          setIsLoading(false);
           return isRemove;
         }
       });
     } catch (e) {
       console.log(e);
+      setIsLoading(true);
     }
   };
 
@@ -235,11 +243,13 @@ const DetailContent = ({ noticeId }: Props) => {
         if (response && response.status === 200) {
           const req = response.data.data.info;
           setData(req);
+          setIsLoading(false);
           return req;
         }
       });
     } catch (error) {
       console.log(error);
+      setIsLoading(true);
     }
   }, [id]);
 
@@ -249,189 +259,202 @@ const DetailContent = ({ noticeId }: Props) => {
     }
   }, []);
 
+  useEffect(() => {
+    return () => {
+      setIsLoading(true);
+      setIsError(false);
+    };
+  }, []);
+
   return (
     <>
       <MetaTag title="Notice | Detial Page" />
 
-      <DetailLayout>
-        {isEdit && (
-          <WriteTitle>
-            <Typography type="h5" color="black">
-              Edit Notice
-            </Typography>
-          </WriteTitle>
-        )}
+      {!isLoading ? (
+        <DetailLayout>
+          {isEdit && (
+            <WriteTitle>
+              <Typography type="h5" color="black">
+                Edit Notice
+              </Typography>
+            </WriteTitle>
+          )}
 
-        <ContentHeadArea>
-          {data && data !== undefined && (
-            <>
-              <TitleBlock>
-                {!isEdit ? (
-                  <>
-                    <Typography type="h4" color="black">
-                      {data.title}
-                    </Typography>
-
-                    <BeforeEditTitle>
-                      <Typography type="b3" color="gray700">
-                        {data.dateCreate !== undefined &&
-                          format(
-                            new Date(data.dateCreate),
-                            'MMM-dd-yyyy h:mm:ss a',
-                          )}
+          <ContentHeadArea>
+            {data && data !== undefined && (
+              <>
+                <TitleBlock>
+                  {!isEdit ? (
+                    <>
+                      <Typography type="h4" color="black">
+                        {data.title}
                       </Typography>
 
-                      <FlexMixin>
-                        <EditButton
-                          size="sm"
-                          color="primary"
-                          variant="outline"
-                          onClick={handleEdit(true)}
-                        >
-                          <Typography type="h6" color="primary700">
-                            Edit
-                          </Typography>
-                        </EditButton>
-
-                        <EditButton
-                          size="sm"
-                          color="primary"
-                          variant="outline"
-                          onClick={handleOpenModal(true)}
-                        >
-                          <Typography type="h6" color="primary700">
-                            Delete
-                          </Typography>
-                        </EditButton>
-                      </FlexMixin>
-                    </BeforeEditTitle>
-                  </>
-                ) : (
-                  <>
-                    <Typography type="b4" color="gray900">
-                      Title :
-                    </Typography>
-
-                    <InsertItem type="title" isInvalid={data.title === ''}>
-                      <ContentTitleInput
-                        value={data.title}
-                        onChange={handleTitle}
-                        ref={inputElement}
-                        size="lg"
-                        width="100%"
-                        autoFocus
-                      />
-
-                      <div>
-                        <Typography
-                          color={data.title === '' ? 'red' : 'primary900'}
-                        >
-                          {data.title === ''
-                            ? 'Please enter title text in input box.'
-                            : 'Title is required.'}
+                      <BeforeEditTitle>
+                        <Typography type="b3" color="gray700">
+                          {data.dateCreate !== undefined &&
+                            format(
+                              new Date(data.dateCreate),
+                              'MMM-dd-yyyy h:mm:ss a',
+                            )}
                         </Typography>
-                      </div>
-                    </InsertItem>
 
-                    <Typography type="b4" color="gray900">
-                      Type :
+                        <FlexMixin>
+                          <EditButton
+                            size="sm"
+                            color="primary"
+                            variant="outline"
+                            onClick={handleEdit(true)}
+                          >
+                            <Typography type="h6" color="primary700">
+                              Edit
+                            </Typography>
+                          </EditButton>
+
+                          <EditButton
+                            size="sm"
+                            color="primary"
+                            variant="outline"
+                            onClick={handleOpenModal(true)}
+                          >
+                            <Typography type="h6" color="primary700">
+                              Delete
+                            </Typography>
+                          </EditButton>
+                        </FlexMixin>
+                      </BeforeEditTitle>
+                    </>
+                  ) : (
+                    <>
+                      <Typography type="b4" color="gray900">
+                        Title :
+                      </Typography>
+
+                      <InsertItem type="title" isInvalid={data.title === ''}>
+                        <ContentTitleInput
+                          value={data.title}
+                          onChange={handleTitle}
+                          ref={inputElement}
+                          size="lg"
+                          width="100%"
+                          autoFocus
+                        />
+
+                        <div>
+                          <Typography
+                            color={data.title === '' ? 'red' : 'primary900'}
+                          >
+                            {data.title === ''
+                              ? 'Please enter title text in input box.'
+                              : 'Title is required.'}
+                          </Typography>
+                        </div>
+                      </InsertItem>
+
+                      <Typography type="b4" color="gray900">
+                        Type :
+                      </Typography>
+
+                      <InsertItem type="type">
+                        <ContentTypeSelect
+                          onChange={handleSelectOption}
+                          value={selected}
+                        >
+                          {selectList.map((item) => (
+                            <option value={item} key={item}>
+                              {item}
+                            </option>
+                          ))}
+                        </ContentTypeSelect>
+                      </InsertItem>
+                    </>
+                  )}
+                </TitleBlock>
+
+                {isEdit && (
+                  <InsertBottomArea>
+                    <Typography type="p4" color="dgray300">
+                      {data.dateCreate !== undefined &&
+                        format(
+                          new Date(data.dateCreate),
+                          'MMM-dd-yyyy h:mm:ss a',
+                        )}
                     </Typography>
-
-                    <InsertItem type="type">
-                      <ContentTypeSelect
-                        onChange={handleSelectOption}
-                        value={selected}
-                      >
-                        {selectList.map((item) => (
-                          <option value={item} key={item}>
-                            {item}
-                          </option>
-                        ))}
-                      </ContentTypeSelect>
-                    </InsertItem>
-                  </>
+                  </InsertBottomArea>
                 )}
-              </TitleBlock>
+              </>
+            )}
+          </ContentHeadArea>
 
-              {isEdit && (
-                <InsertBottomArea>
-                  <Typography type="p4" color="dgray300">
-                    {data.dateCreate !== undefined &&
-                      format(
-                        new Date(data.dateCreate),
-                        'MMM-dd-yyyy h:mm:ss a',
-                      )}
-                  </Typography>
-                </InsertBottomArea>
-              )}
-            </>
-          )}
-        </ContentHeadArea>
+          <InsertArea>
+            {!isEdit ? (
+              <>
+                <ContentDescBox>
+                  {data.content !== '' ? <Markup content={data.content} /> : ''}
+                </ContentDescBox>
+                <ButtonArea>
+                  <ClickButton
+                    size="md"
+                    color="primary"
+                    variant="solid"
+                    onClick={viewlist}
+                  >
+                    <Typography type="b3" color="atlantic">
+                      View list
+                    </Typography>
+                  </ClickButton>
+                </ButtonArea>
+              </>
+            ) : (
+              <>
+                <EditorBox>
+                  <TUI
+                    htmlStr={data.content as string}
+                    setHtmlStr={setContents}
+                    autofocus={false}
+                  />
+                </EditorBox>
 
-        <InsertArea>
-          {!isEdit ? (
-            <>
-              <ContentDescBox>
-                {data.content !== '' ? <Markup content={data.content} /> : ''}
-              </ContentDescBox>
-              <ButtonArea>
-                <ClickButton
-                  size="md"
-                  color="primary"
-                  variant="solid"
-                  onClick={viewlist}
-                >
-                  <Typography type="b3" color="atlantic">
-                    View list
-                  </Typography>
-                </ClickButton>
-              </ButtonArea>
-            </>
-          ) : (
-            <>
-              <EditorBox>
-                <TUI
-                  htmlStr={data.content as string}
-                  setHtmlStr={setContents}
-                  autofocus={false}
-                />
-              </EditorBox>
+                <UploadButtonBlock>
+                  <ClickButton
+                    size="md"
+                    color="primary"
+                    variant="outline"
+                    onClick={handleEdit(false)}
+                  >
+                    <Typography type="h6" color="primary700">
+                      Cancel
+                    </Typography>
+                  </ClickButton>
 
-              <UploadButtonBlock>
-                <ClickButton
-                  size="md"
-                  color="primary"
-                  variant="outline"
-                  onClick={handleEdit(false)}
-                >
-                  <Typography type="h6" color="primary700">
-                    Cancel
-                  </Typography>
-                </ClickButton>
-
-                <ClickButton
-                  size="md"
-                  color="primary"
-                  variant="solid"
-                  onClick={handleUploadOpenModal(true)}
-                  disabled={(data.title === '' || data.content === '') && true}
-                >
-                  <Typography
-                    type="h6"
-                    color={
-                      data.title === '' || data.content === ''
-                        ? 'gray700'
-                        : 'atlantic'
+                  <ClickButton
+                    size="md"
+                    color="primary"
+                    variant="solid"
+                    onClick={handleUploadOpenModal(true)}
+                    disabled={
+                      (data.title === '' || data.content === '') && true
                     }
                   >
-                    Edit
-                  </Typography>
-                </ClickButton>
-              </UploadButtonBlock>
-            </>
-          )}
-        </InsertArea>
-      </DetailLayout>
+                    <Typography
+                      type="h6"
+                      color={
+                        data.title === '' || data.content === ''
+                          ? 'gray700'
+                          : 'atlantic'
+                      }
+                    >
+                      Edit
+                    </Typography>
+                  </ClickButton>
+                </UploadButtonBlock>
+              </>
+            )}
+          </InsertArea>
+        </DetailLayout>
+      ) : (
+        <Loading />
+      )}
 
       {isOpen && (
         <DeleteModal
